5f2d1cb1cf2ad0c4bb6f63831593a19836d22f3b
Akash Singh <akashsinghit2024@gmail.com>
2023-08-13 11:48:35 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navbar />
     </div>
   );
 }
